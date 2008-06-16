99e7580a903e1433c6b92082f5da3ae8592238dd
Jesse Keating <jkeating@redhat.com>
2008-06-16 17:28:41 -0400

    Whoops, lets make this 1.2.18.1 instead of 1.2.19.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import glob
 
 setup(name='pungi',
-      version='1.2.19',
+      version='1.2.18.1',
       description='Distribution compose tool',
       author='Jesse Keating',
       author_email='jkeating@redhat.com',
